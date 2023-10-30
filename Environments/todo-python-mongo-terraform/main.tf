fd99b23a944216f25a9094a3637a811c03e261ad
Senyang Cai (MSFT) <57525824+dotnet-7@users.noreply.github.com>
2023-10-30 15:14:45 +0800

    Update main.tf

--- Environments/todo-python-mongo-terraform/main.tf
+++ Environments/todo-python-mongo-terraform/main.tf
@@ -1,25 +1,3 @@
-variable "location" {
-  description = "The supported Azure location where the resource deployed"
-  type        = string
-}
-
-variable "environment_name" {
-  description = "The name of the azd environment to be deployed"
-  type        = string
-}
-
-variable "principal_id" {
-  description = "The Id of the azd service principal to add to deployed keyvault access policies"
-  type        = string
-  default     = ""
-}
-
-variable "useAPIM" {
-  description = "Flag to use Azure API Management to mediate the calls between the Web frontend and the backend API."
-  type        = bool
-  default     = false
-}
-
 locals {
   tags                         = { azd-env-name : var.environment_name }
   sha                          = base64encode(sha256("${var.environment_name}${var.location}${data.azurerm_client_config.current.subscription_id}"))
