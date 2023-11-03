5931b96f47498b1e1a87aa4bca73870a675cf445
Senyang Cai (WICRESOFT NORTH AMERICA LTD) <v-senyangcai@microsoft.com>
2023-11-03 10:19:19 +0800

    change

--- Environments/terraform-test2/function_app.tf
+++ Environments/terraform-test2/function_app.tf
@@ -1,43 +1,48 @@
-variable "environment_name" {
-  description = "The name of the environment"
-  type        = string
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.75"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
 }
 
 variable "location" {
-  description = "The location of the resource"
+  description = "The supported Azure location where the resource deployed"
   type        = string
 }
 
-locals {
-  tags                         = { azd-env-name : var.environment_name }
-  sha                          = base64encode(sha256("${var.environment_name}${var.location}${data.azurerm_client_config.current.subscription_id}"))
-  resource_token               = substr(replace(lower(local.sha), "[^A-Za-z0-9_]", ""), 0, 13)
-  api_command_line             = "gunicorn --workers 4 --threads 2 --timeout 60 --access-logfile \"-\" --error-logfile \"-\" --bind=0.0.0.0:8000 -k uvicorn.workers.UvicornWorker todo.app:app"
-  cosmos_connection_string_key = "AZURE-COSMOS-CONNECTION-STRING"
-}
-# ------------------------------------------------------------------------------------------------------
-# Deploy resource Group
-# ------------------------------------------------------------------------------------------------------
-resource "azurecaf_name" "rg_name" {
-  name          = var.environment_name
-  resource_type = "azurerm_resource_group"
-  random_length = 0
-  clean_input   = true
+variable "environment_name" {
+  description = "The name of the azd environment to be deployed"
+  type        = string
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = azurecaf_name.rg_name.result
-  location = var.location
+module "key_vault" {
+  source = "github.com/Azure/azure-data-labs-modules/terraform/key-vault"
 
-  tags = local.tags
-}
+  basename                 = "senyangtestkv"
+  resource_group_name      = var.environment_name
+  location                 = var.location
+  sku_name                 = "premium"
+  purge_protection_enabled = false
 
-module "applicationinsights" {
-  source           = "./modules/applicationinsights"
-  location         = var.location
-  rg_name          = azurerm_resource_group.rg.name
-  environment_name = var.environment_name
-  workspace_id     = module.loganalytics.LOGANALYTICS_WORKSPACE_ID
-  tags             = azurerm_resource_group.rg.tags
-  resource_token   = local.resource_token
+  subnet_id            = null
+  private_dns_zone_ids = null
+
+  is_private_endpoint = false
+
+  tags = {
+    Owner       = "Azure Data Labs"
+    Project     = "Azure Data Labs"
+    Environment = "dev"
+  }
 }
